d145dd476f974568219a7c513030c9792a620f4f
Terence Denzil Monteiro <terence@sanjosesolutions.in>
2014-08-30 13:52:59 +0530

    Removed redirect to siblings/admin from siblings/delete

--- protected/controllers/SiblingsController.php
+++ protected/controllers/SiblingsController.php
@@ -104,10 +104,6 @@ class SiblingsController extends Controller
 	public function actionDelete($id)
 	{
 		$this->loadModel($id)->delete();
-
-		// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
-		if(!isset($_GET['ajax']))
-			$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
 	}
 
 	/**
